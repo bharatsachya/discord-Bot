fed09f18c1a5e6a78fa26782792d3ee6d7d3cae5
Anuj Sharma <anujsharmawork11@gmail.com>
2023-10-05 02:04:52 +0530

    saving guild and their prefixesgit add .

--- class/Me.py
+++ class/Me.py
@@ -9,11 +9,6 @@ class Me(commands.Cog):
     async def on_ready(self):
         print("Me.py is ready")
 
-    # @commands.command()
-    # async def me(self,ctx):
-    #     botLatency=round(self.client.latency*1000)
-    #     await ctx.send(f"{botLatency}ms")
-
     @commands.command(aliases=['myself','pointtome'])
     async def me(self,ctx):
         embedmessage=discord.Embed(title="Who are you?!", description="Meet this mf", color=discord.Color.gold())
@@ -21,8 +16,18 @@ class Me(commands.Cog):
         embedmessage.set_author(name=f"{ctx.author.display_name}", icon_url=ctx.author.avatar)
         embedmessage.set_thumbnail(url=ctx.guild.icon)
         embedmessage.set_image(url=ctx.author.avatar)
-        embedmessage.add_field(name="fieldname",value="field value", inline=False)
         embedmessage.set_footer(text="👆👆👆This is your image👆👆👆",icon_url=ctx.author.avatar)
+
+        await ctx.send(embed=embedmessage)
+
+    @commands.command(aliases=['member'])
+    async def mem(self,ctx,member:discord.Member):
+        embedmessage=discord.Embed(title=member.display_name, description="Meet this mf", color=discord.Color.gold())
+
+        embedmessage.set_author(name=f"{member.display_name}", icon_url=member.avatar)
+        embedmessage.set_thumbnail(url=ctx.guild.icon)
+        embedmessage.set_image(url=member.avatar)
+        embedmessage.set_footer(text="👆👆👆This is your image👆👆👆",icon_url=member.avatar)
 
         await ctx.send(embed=embedmessage)
 
